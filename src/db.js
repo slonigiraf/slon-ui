4eeca60b4c3c69091cc5d8bede6f42841a795a55
Reshetov Denis <reshetovdenis@gmail.com>
2022-03-31 09:50:50 +0500

    Dexie config

--- src/db.js
+++ src/db.js
@@ -2,5 +2,31 @@ import Dexie from 'dexie';
 
 export const db = new Dexie('SLON');
 db.version(1).stores(
-    { issuedRecommendations: '++id, cid, workerPublicKeyHex, reimbursement, refereeSignOverPrivateData, refereeSignOverPublicData', },
+    { recommendationsIssuedByMe: '++id, cid, reimbursement, workerRecommendationSpecificPublicKeyHex, refereeSignOverPrivateData, refereeSignOverPublicData', },
+    { recommendationsAboutMe: 'id, cid, reimbursement, refereePublicKeyHex, workerRecommendationSpecificPublicKeyHex, refereeSignOverPrivateData, refereeSignOverPublicData', },
+    { recommendationsAboutMyWorkers: '++id, cid, reimbursement, refereePublicKeyHex, workerRecommendationSpecificPublicKeyHex, refereeSignOverPublicData, workerSign', },
 );
+
+const addRecommendationsIssuedByMe = async (id, event) => {
+    event.preventDefault()
+    const name = document.querySelector('.item-name').value
+    const price = document.querySelector('.item-price').value
+    await db.items.add({
+      name,
+      price: Number(price),
+      itemHasBeenPurchased: false
+    })
+  }
+  
+  const removeItemFromDb = async id => {
+    await db.items.delete(id)
+  }
+  
+  const markAsPurchased = async (id, event) => {
+    if (event.target.checked) {
+      await db.items.update(id, {itemHasBeenPurchased: true})
+    }
+    else {
+      await db.items.update(id, {itemHasBeenPurchased: false})
+    }
+  }
